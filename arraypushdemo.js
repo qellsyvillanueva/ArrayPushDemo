f3cc44fb767b65f6b07052dc9a440164a50000cb
qellsyvillanueva <42422251+qellsyvillanueva@users.noreply.github.com>
2018-08-15 12:11:43 -1000

    Add files via upload

---
+++ arraypushdemo.js
@@ -0,0 +1,15 @@
+//build an array that stores first names. The default value of the array will be empty
+var joseph = [
+  ];
+var landon = [
+ ];
+// ask user for first name, and store it into the first names array.
+	var first = prompt("What is your first name?");
+  jospeh = first;
+//Ask usrer for a last name, and store it into the last names array
+	var last = prompt("What is your last name?");
+  landon = last;
+//display to the window both first and last name from the array with the length of the both arrays
+
+window.alert(first[0] + last[0] + first.length + last.length);
+
